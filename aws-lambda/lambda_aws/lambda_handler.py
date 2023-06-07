d4fcf5d71317995b179d053762fd4498068ce96b
newdevon <stvchan999@gmail.com>
2023-06-07 12:02:16 -0400

    written lambda functionality for single csv file

---
+++ aws-lambda/lambda_aws/lambda_handler.py
@@ -0,0 +1,74 @@
+import boto3
+import pymysql
+from .. import config
+
+s3_cient = boto3.client('s3')
+
+# Read CSV file content from S3 bucket
+def read_data_from_s3(event):
+    bucket_name = event["Records"][0]["s3"]["bucket"]["name"]
+    s3_file_name = event["Records"][0]["s3"]["object"]["key"]
+    print(f"Bucket Name: {bucket_name}, S3 File:  {s3_file_name}")
+    resp = s3_cient.get_object(Bucket=bucket_name, Key=s3_file_name)
+
+    data = resp['Body'].read().decode('utf-8')
+    data = data.split("\n")
+    return data
+
+def lambda_handler(event, context):
+    rds_endpoint  = config.rds_endpoint
+    username = config.username
+    password = config.password # RDS Mysql password
+    db_name = config.db_name # RDS MySQL DB name
+    connection = None
+    try:
+        connection = pymysql.connect(host=rds_endpoint, user=username, passwd=password, db=db_name, connect_timeout=5)
+    except pymysql.MySQLError as e:
+        print("ERROR: Unexpected error: Could not connect to MySQL instance.")
+
+
+    # tables_insert = {
+    #     'student.csv': 'INSERT INTO student (empid, empname, empaddress) VALUES (%s, %s, %s)',
+    #     'teacher.csv': 'INSERT INTO teacher (empid, empname, empaddress) VALUES (%s, %s, %s)',
+    #     'course.csv': 'INSERT INTO course (empid, empname, empaddress) VALUES (%s, %s, %s)',
+    #     'grade.csv': 'INSERT INTO grade (empid, empname, empaddress) VALUES (%s, %s, %s)',
+    #     'admin.csv': 'INSERT INTO admin (empid, empname, empaddress) VALUES (%s, %s, %s)'
+    # }
+
+
+
+    try:
+        cursor = connection.cursor()
+
+        sql_script1 = "DROP TABLE IF EXISTS teacher;" 
+        cursor.execute(sql_script1)
+        connection.commit() #commit the execution
+
+        sql_script2 = "CREATE TABLE teacher (id SERIAL PRIMARY KEY, name varchar(50), email varchar(50));"
+        cursor.execute(sql_script2)
+        connection.commit() #commit the execution
+        print("Created 'teacher' Table")
+        
+    except Exception as e:
+        print(e)
+
+    data = read_data_from_s3(event)
+
+    with connection.cursor() as cur:
+        for person in data: # Iterate over S3 csv file content and insert into MySQL database
+            try:
+                person = person.replace("\n","").split(",")
+                #print(person)
+                #print (">>>>>>>"+str(person))
+                cur.execute('INSERT INTO teacher (name, email) VALUES ("'+str(person[1])+'", "'+str(person[2])+'")')
+                connection.commit()
+                #print("inserted record")
+            except Exception as e:
+                print(e)
+                continue
+        cur.execute("select * from teacher")
+        # Display teacher table records
+        # for row in cur:
+        #     print (row)
+    if connection:
+        connection.commit()
